04082ef21388b4bc9268c9e64cbef583f1f02d6e
Beto <beto@beto.cat>
2023-04-01 23:36:18 +0200

    Creo rama Refactorizacion y añado mi nombre e iniciales a todos los metodos.

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,31 +3,31 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_coche_su_velocidad_es_cero(){
+    public void test_al_crear_coche_su_velocidad_es_cero_AlbertPB(){
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0,nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_acelerar_coche_su_velocidad_aumenta(){
+    public void test_al_acelerar_coche_su_velocidad_aumenta_AlbertPB(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerar_AlbertPB(30);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_decelerar_coche_su_velocidad_disminuye(){
+    public void test_al_decelerar_coche_su_velocidad_disminuye_AlbertPB(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad=50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerar_AlbertPB(20);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_decelerar_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+    public void test_al_decelerar_coche_su_velocidad_no_puede_ser_menor_que_cero_AlbertPB(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad=50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerar_AlbertPB(80);
         Assertions.assertEquals(0,nuevoCoche.velocidad);
     }
 
